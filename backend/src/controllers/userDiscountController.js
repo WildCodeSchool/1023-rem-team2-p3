e2430dec2480f0533d1d489c4239d56e8f12cea2
YoussefSlim <slimyoussef9511@gmail.com>
2024-03-01 08:44:54 +0100

    test commit

--- backend/src/controllers/userDiscountController.js
+++ backend/src/controllers/userDiscountController.js
@@ -1,6 +1,3 @@
-/* eslint-disable camelcase */
-// eslint-disable-next-line consistent-return
-/* eslint-disable no-unused-vars */
 const tables = require("../tables");
 
 const getUserDiscount = async (req, res, next) => {
@@ -10,9 +7,10 @@ const getUserDiscount = async (req, res, next) => {
     const [admin] = await tables.user.getUserById(id);
 
     if (admin[0].is_admin !== "admin" && admin[0].is_admin !== "superAdmin") {
-      return res.status(401).json({ error: "Vous n'avez pas les droits" });
+      res.status(401).json({ error: "Vous n'avez pas les droits" });
+    } else {
+      res.json(userDiscount);
     }
-    res.json(userDiscount);
   } catch (err) {
     next(err);
   }
@@ -23,7 +21,7 @@ const addUserDiscount = async (req, res, next) => {
     const id = req.payload;
     const add = req.body;
     const discountAll = await tables.discount.getDiscountAll();
-    const userDiscount = await tables.user_discount.getUserDiscountAll();
+    // const userDiscount = await tables.user_discount.getUserDiscountAll();
     const [test] = await tables.user_discount.getIdController({
       user_id: id,
       discount_id: add.discount_id,
@@ -38,20 +36,20 @@ const addUserDiscount = async (req, res, next) => {
           res.json({
             message: "Code promo déjà utilisé par cet utilisateur",
           });
+        } else if (discountAll[add.discount_id - 1].quantity > 0) {
+          const updateResult = await tables.discount.decrementdiscountQuantity({
+            discount_id: add.discount_id,
+          });
+          const result = await tables.user_discount.addUserDiscount(
+            id,
+            add.discount_id
+          );
+          res.json({ message: "Code promos utilisé" });
         } else {
-          if (discountAll[add.discount_id - 1].quantity > 0) {
-            const updateResult =
-              await tables.discount.decrementdiscountQuantity({
-                discount_id: add.discount_id,
-              });
-            const result = await tables.user_discount.addUserDiscount(id, add.discount_id);
-            res.json({ message: "Code promos utilisé" });
-          } else {
-            const up = await tables.discount.statusDiscount({
-              discount_id: add.discount_id,
-            });
-            res.json({ message: "Code promos invalide" });
-          }
+          const up = await tables.discount.statusDiscount({
+            discount_id: add.discount_id,
+          });
+          res.json({ message: "Code promos invalide" });
         }
       } else {
         const up = await tables.discount.statusDiscount({
