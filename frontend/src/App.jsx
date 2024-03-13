e596e69db69c7e82d0435f2d9b4b4a6fc39446fa
asitouze <aurelien.sitouze@gmail.com>
2024-03-13 09:37:39 +0100

    commit for canvas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,10 @@
+import ParticleBackground from "./components/Canvas/Canvas";
+
 function App() {
   return (
-    <div className="font-secondary_font min-h-[calc(100vh-160px)] flex flex-wrap justify-center bg-gradient-to-b from-background-color-second to-background-color-first">
-      App
+    <div className="font-secondary_font min-h-[calc(100vh-160px)] flex flex-wrap justify-center bg-gradient-to-b from-background-color-second to-background-color-first ">
+      <h1 className="absolute text-white top-40 text-7xl"> The Lab </h1>
+      <ParticleBackground />
     </div>
   );
 }
