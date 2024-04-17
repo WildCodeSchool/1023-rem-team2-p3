06e6c7499d901da2f227559ebf56815b8258b1d1
asitouze <aurelien.sitouze@gmail.com>
2024-04-17 16:36:37 +0200

    finish code promo backoffice

--- frontend/src/pages/BackOfficePages/CodePromo.jsx
+++ frontend/src/pages/BackOfficePages/CodePromo.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import Header from "../../components/DashBoard/Header";
 import Sidebar from "../../components/DashBoard/Sidebar";
+import PromoCode from "../../components/DashBoard/PromoCode/PromoCode";
 
 export default function Notes() {
   return (
-    <div className=" text-white font-secondary-font bg-[#281f31] w-[81rem] lg:h-auto rounded-[20px] my-10 ">
+    <div className="flex flex-col text-white font-secondary-font bg-[#281f31] lg:h-auto rounded-[20px] my-10 ">
       <Header />
-      <div className="flex">
+      <div className="flex flex-row">
         <Sidebar />
+        <PromoCode />
       </div>
     </div>
   );
