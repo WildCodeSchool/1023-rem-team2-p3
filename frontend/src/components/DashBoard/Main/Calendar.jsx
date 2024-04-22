23ca62863ad10edf275ae562c75534f4c5c8f079
Alekshamon <aleksandrahamon@gmail.com>
2024-04-22 09:13:20 +0200

    dashboard homepage done

--- frontend/src/components/DashBoard/Main/Calendar.jsx
+++ frontend/src/components/DashBoard/Main/Calendar.jsx
@@ -1,61 +1,118 @@
-/* eslint-disable import/no-extraneous-dependencies */
 import React, { useEffect, useState } from "react";
-import Calendar from "react-calendar";
-import { isSameDay } from "date-fns";
+import {
+  format,
+  isSameMonth,
+  isSameDay,
+  startOfMonth,
+  addDays,
+  subMonths,
+  addMonths,
+  getDay,
+} from "date-fns";
 
-export default function MainBackoffice() {
-  const [events, setEvents] = useState([]);
+export default function CalendarComponent() {
   const [selectedDate, setSelectedDate] = useState(new Date());
-  const [selectedEvent, setSelectedEvent] = useState(null);
-  console.info("events:", events);
-  console.info("selectedDate:", selectedDate);
-  console.info("selectedEvent:", selectedEvent);
+  const [events, setEvents] = useState([]);
 
   useEffect(() => {
     fetch(`${import.meta.env.VITE_BACKEND_URL}/api/events`)
       .then((response) => response.json())
       .then((data) => {
         setEvents(data);
-        console.info("data:", data);
       })
       .catch((error) => console.error("Error:", error));
   }, []);
 
-  const handleDateClick = (date) => {
-    const event = events.find((event) => isSameDay(new Date(event.date), date));
-    setSelectedEvent(event || null);
+  const daysInMonth = (date) => {
+    const firstDayOfMonth = startOfMonth(date);
+    const days = [];
+    // Trouve le jour de la semaine du premier jour du mois (0 pour dimanche, 1 pour lundi, ..., 6 pour samedi)
+    const firstDayOfWeek = getDay(firstDayOfMonth);
+    // Ajoute les jours du mois précédent si nécessaire pour compléter la première semaine
+    for (let i = firstDayOfWeek === 0 ? 6 : firstDayOfWeek - 1; i > 0; i--) {
+      const prevDay = addDays(firstDayOfMonth, -i);
+      days.push({ date: prevDay, isOutsideMonth: true });
+    }
+    // Ajoute les jours du mois en cours
+    for (let i = 0; i < 31; i++) {
+      const day = addDays(firstDayOfMonth, i);
+      days.push({ date: day, isOutsideMonth: false });
+      if (!isSameMonth(day, date)) break;
+    }
+    return days;
+  };
+
+  const handleDateClick = (day) => {
+    setSelectedDate(day);
+  };
+
+  const eventDates = events.map((event) => new Date(event.date));
+
+  const handlePrevMonth = () => {
+    setSelectedDate(subMonths(selectedDate, 1));
+  };
+
+  const handleNextMonth = () => {
+    setSelectedDate(addMonths(selectedDate, 1));
   };
 
   return (
-    <div className="flex flex-col justify-center items-center p-4 ">
-      <h1 className="font-primary-font text-center p-4 text-[20px]">
-        Calendrier des événements
-      </h1>
-      <Calendar
-        onChange={setSelectedDate}
-        value={selectedDate}
-        className={(date) => {
-          if (!events) {
-            return "bg-background-color-second rounded-lg shadow p-4 mx-8 w-96 h-52";
-          }
-          const eventDates = events.map((event) => new Date(event.date));
-          return eventDates.find((d) => isSameDay(d, date))
-            ? "text-pink-500 bg-blue-500 rounded-full"
-            : "bg-background-color-second rounded-lg shadow p-4 mx-8 w-96 h-52";
-        }}
-        onClickDay={handleDateClick}
-      />
-      {selectedEvent && (
-        <div className="mt-4">
-          <h2 className="text-lg font-semibold">
-            Événement du {selectedEvent.date}
-          </h2>
-          <p>Ville : {selectedEvent.city}</p>
-          <p>Adresse : {selectedEvent.address}</p>
-          <p>Quantité : {selectedEvent.quantity}</p>
-          <p>Statut : {selectedEvent.status}</p>
+    <div className="flex flex-col justify-center items-center p-4">
+      <div className="font-primary-font text-center p-4 text-[20px]">
+        <span>Calendrier des événements</span>
+      </div>
+      <div className="flex flex-col items-center">
+        <div className="flex items-center mb-2">
+          <button onClick={handlePrevMonth} className="w-10">
+            &#8249;
+          </button>
+          <div>{format(selectedDate, "MMMM yyyy")}</div>
+          <button onClick={handleNextMonth} className="w-10">
+            &#8250;
+          </button>
         </div>
-      )}
+        <div className="border border-gray-200 rounded-lg p-4">
+          <div className="grid grid-cols-7 gap-1 mb-2 w-[500px]">
+            {["Lun", "Mar", "Mer", "Jeu", "Ven", "Sam", "Dim"].map(
+              (dayName) => (
+                <div key={dayName} className="text-xs text-gray-500 font-bold">
+                  {dayName}
+                </div>
+              )
+            )}
+          </div>
+          <div className="grid grid-cols-7 gap-1">
+            {daysInMonth(selectedDate).map(({ date, isOutsideMonth }) => (
+              <div
+                key={date.toString()}
+                className={`px-2 py-1 cursor-pointer ${isSameDay(date, selectedDate) ? "bg-violet-500 text-white" : ""} ${
+                  eventDates.some((eventDate) => isSameDay(eventDate, date))
+                    ? "bg-red-500 text-white"
+                    : ""
+                } ${isOutsideMonth ? "text-gray-400" : ""}`}
+                onClick={() => handleDateClick(date)}
+                onKeyDown={(e) => {
+                  if (e.key === "Enter" || e.key === " ") {
+                    handleDateClick(date);
+                  }
+                }}
+                tabIndex={0} // Add tabIndex attribute to make the element focusable
+                role="button" // Add role attribute to indicate that the element is interactive
+              >
+                {format(date, "d")}
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="m-5">
+          <span className="rounded-full p-1 bg-violet-500 text-white">
+            Date actuelle
+          </span>{" "}
+          <span className="rounded-full p-1 bg-red-500  text-white">
+            Date d'événements
+          </span>
+        </div>
+      </div>
     </div>
   );
 }
--- frontend/src/components/DashBoard/Main/FuturesEvents.jsx
+++ frontend/src/components/DashBoard/Main/FuturesEvents.jsx
@@ -18,33 +18,40 @@ export default function FuturesEvents() {
       .catch((error) => console.error("Error:", error));
   }, [events]);
   return (
-    <div className="text-white  flex flex-col justify-center items-center font-secondary-font text-[16px] m-10 ">
-      {events.map((event, index) => (
-        <div
-          // eslint-disable-next-line react/no-array-index-key
-          key={index}
-          className=" flex justify-between items-center border border-white font-[100] bg-background-color-second cursor-pointer ease-in duration-100 p-5 rounded-[20px] m-1  md:w-[700px] md:h-[60px] w-[500px]  "
-        >
-          <h3>
-            <LuMapPin className="inline-block " /> {event.city}
-          </h3>
-          <p>
-            <MdOutlineDateRange className="inline-block" />{" "}
-            <span className="font-secondary-font bg-clip-text text-transparent  text-white">
-              {new Date(event.date).toLocaleDateString("fr-FR")}
-            </span>
-          </p>
-          <p>
-            <CiMapPin className="inline-block " /> {event.address}
-          </p>
-          <p className="md:flex md:justify-end md:gap-2">
-            <IoTicketOutline className="inline-block " />
-            <span className="font-secondary-font  font-bold bg-clip-text text-transparent  text-white">
-              {event.quantity}
-            </span>
-          </p>
-        </div>
-      ))}
-    </div>
+    <>
+      <div>
+        <h1 className="flex flex-col items-center text-white font-primary-font text-[24px] m-4">
+          Derniers évènements
+        </h1>
+      </div>
+      <div className="text-white  flex flex-col justify-center items-center font-secondary-font text-[16px] mx-10 my-5 ">
+        {events.map((event, index) => (
+          <div
+            // eslint-disable-next-line react/no-array-index-key
+            key={index}
+            className=" flex justify-between items-center border border-white font-[100] bg-background-color-second cursor-pointer ease-in duration-100 p-5 rounded-[20px] m-1  md:w-[700px] md:h-[60px] w-[500px]  "
+          >
+            <h3>
+              <LuMapPin className="inline-block " /> {event.city}
+            </h3>
+            <p>
+              <MdOutlineDateRange className="inline-block" />{" "}
+              <span className="font-secondary-font bg-clip-text text-transparent  text-white">
+                {new Date(event.date).toLocaleDateString("fr-FR")}
+              </span>
+            </p>
+            <p>
+              <CiMapPin className="inline-block " /> {event.address}
+            </p>
+            <p className="md:flex md:justify-end md:gap-2">
+              <IoTicketOutline className="inline-block " />
+              <span className="font-secondary-font  font-bold bg-clip-text text-transparent  text-white">
+                {event.quantity}
+              </span>
+            </p>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
