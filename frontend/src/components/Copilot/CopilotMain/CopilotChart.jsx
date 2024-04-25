95d4fa593af7efd1f734211e633df16fcb36d616
Alekshamon <aleksandrahamon@gmail.com>
2024-04-25 16:40:44 +0200

    copilot chart done fixed

--- frontend/src/components/Copilot/CopilotMain/CopilotChart.jsx
+++ frontend/src/components/Copilot/CopilotMain/CopilotChart.jsx
@@ -32,107 +32,111 @@ export default function CopilotChart() {
     <div className="flex flex-col justify-center items-center h-full">
       <h2 className="text-2xl font-primary-font mb-4">Mes performances</h2>
       <div className="flex flex-wrap justify-center gap-4 mx-10 relative">
-        <div className="absolute hidden md:block top-0 bottom-0 left-[-40px] w-1 bg-white" />
-        <div className="absolute  hidden md:block top-0 left-[-40px] mt-0.5 ml-3 text-xs text-white">
-          100
-        </div>
-        <div className="absolute hidden md:block bottom-0left-[-40px] mb-0.5 ml-3 text-xs text-white">
-          0
-        </div>
+        {notes.length > 0 && (
+          <>
+            <div className="absolute hidden md:block top-0 bottom-0 left-[-40px] w-1 bg-white" />
+            <div className="absolute hidden md:block top-0 left-[-40px] mt-0.5 ml-3 text-xs text-white">
+              100
+            </div>
+            <div className="absolute hidden md:block bottom-0 left-[-40px] mb-0.5 ml-3 text-xs text-white">
+              0
+            </div>
+          </>
+        )}
         {notes.map((note, index) => (
           <div className="flex flex-col">
             <div key={index} className="flex items-center gap-8 md:gap-12">
               <div className="w-1 h-28 md:w-3 md:h-64 bg-gray-200 rounded-lg overflow-hidden relative">
                 <div
                   className={`w-full ${getBarColor(note.note_physique)} transition-all absolute bottom-0`}
-                  style={{ height: `${note.note_physique}%` }}
+                  style={{ height: `${note.note_physique || 0}%` }}
                 />
                 <span className="absolute bottom-full left-1/2 transform -translate-x-1/2">
-                  {note.note_physique}
+                  {note.note_physique || 0}
                 </span>
               </div>
 
               <div className="w-1 h-28 md:w-3 md:h-64 bg-gray-200 rounded-lg overflow-hidden relative">
                 <div
                   className={`w-full ${getBarColor(note.note_vitesse)} transition-all absolute bottom-0`}
-                  style={{ height: `${note.note_vitesse}%` }}
+                  style={{ height: `${note.note_vitesse || 0}%` }}
                 />
                 <span className=" absolute bottom-full left-1/2 transform -translate-x-1/2">
-                  {note.note_vitesse}
+                  {note.note_vitesse || 0}
                 </span>
               </div>
 
               <div className="w-1 h-28 md:w-3 md:h-64 bg-gray-200 rounded-lg overflow-hidden relative">
                 <div
                   className={`w-full ${getBarColor(note.note_passe)} transition-all absolute bottom-0`}
-                  style={{ height: `${note.note_passe}%` }}
+                  style={{ height: `${note.note_passe || 0}%` }}
                 />
                 <span className=" absolute bottom-full left-1/2 transform -translate-x-1/2">
-                  {note.note_passe}
+                  {note.note_passe || 0}
                 </span>
               </div>
 
               <div className="w-1 h-28 md:w-3 md:h-64 bg-gray-200 rounded-lg overflow-hidden relative">
                 <div
                   className={`w-full ${getBarColor(note.note_tir)} transition-all absolute bottom-0`}
-                  style={{ height: `${note.note_tir}%` }}
+                  style={{ height: `${note.note_tir || 0}%` }}
                 />
                 <span className=" absolute bottom-full left-1/2 transform -translate-x-1/2">
-                  {note.note_tir}
+                  {note.note_tir || 0}
                 </span>
               </div>
               <div className="w-1 h-28 md:w-3 md:h-64 bg-gray-200 rounded-lg overflow-hidden relative">
                 <div
                   className={`w-full ${getBarColor(note.note_dribble)} transition-all absolute bottom-0`}
-                  style={{ height: `${note.note_dribble}%` }}
+                  style={{ height: `${note.note_dribble || 0}%` }}
                 />
                 <span className=" absolute bottom-full left-1/2 transform -translate-x-1/2">
-                  {note.note_dribble}
+                  {note.note_dribble || 0}
                 </span>
               </div>
               <div className="w-1 h-28 md:w-3 md:h-64 bg-gray-200 rounded-lg overflow-hidden relative">
                 <div
                   className={`w-full ${getBarColor(note.note_vista)} transition-all absolute bottom-0`}
-                  style={{ height: `${note.note_vista}%` }}
+                  style={{ height: `${note.note_vista || 0}%` }}
                 />
                 <span className=" absolute bottom-full left-1/2 transform -translate-x-1/2">
-                  {note.note_vista}
+                  {note.note_vista || 0}
                 </span>
               </div>
               <div className="w-1 h-28 md:w-3 md:h-64 bg-gray-200 rounded-lg overflow-hidden relative">
                 <div
                   className={`w-full ${getBarColor(note.note_cf)} transition-all absolute bottom-0`}
-                  style={{ height: `${note.note_cf}%` }}
+                  style={{ height: `${note.note_cf || 0}%` }}
                 />
                 <span className=" absolute bottom-full left-1/2 transform -translate-x-1/2">
-                  {note.note_cf}
+                  {note.note_cf || 0}
                 </span>
               </div>
               <div className="w-1 h-28 md:w-3 md:h-64 bg-gray-200 rounded-lg overflow-hidden relative">
                 <div
                   className={`w-full ${getBarColor(note.note_plongeon)} transition-all absolute bottom-0`}
-                  style={{ height: `${note.note_plongeon}%` }}
+                  style={{ height: `${note.note_plongeon || 0}%` }}
                 />
                 <span className=" absolute bottom-full left-1/2 transform -translate-x-1/2">
-                  {note.note_plongeon}
+                  {note.note_plongeon || 0}
                 </span>
               </div>
               <div className="w-1 h-28 md:w-3 md:h-64 bg-gray-200 rounded-lg overflow-hidden relative">
                 <div
                   className={`w-full ${getBarColor(note.note_dega)} transition-all absolute bottom-0`}
-                  style={{ height: `${note.note_dega}%` }}
+                  style={{ height: `${note.note_dega || 0}%` }}
                 />
                 <span className=" absolute bottom-full left-1/2 transform -translate-x-1/2">
-                  {note.note_dega}
+                  {note.note_dega || 0}
                 </span>
               </div>
               <div className="w-1 h-28 md:w-3 md:h-64 bg-gray-200 rounded-lg overflow-hidden relative">
                 <div
                   className={`w-full ${getBarColor(note.note_pied_faible)} transition-all absolute bottom-0`}
-                  style={{ height: `${note.note_pied_faible}%` }}
+                  style={{ height: `${note.note_pied_faible || 0}%` }}
                 />
                 <span className=" absolute bottom-full left-1/2 transform -translate-x-1/2">
-                  {note.note_pied_faible}
+                  {note.note_pied_faible || 0}
                 </span>
               </div>
             </div>
